1bac407c7034c4caad33cb5a46be8e3d88a7efae
shadabahmad01 <shdbtp322@gmail.com>
2022-02-06 15:11:58 +0530

    deployment changes

--- studentpost/settings.py
+++ studentpost/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = str(os.getenv('SECRET_KEY'))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
--- studentpost/wsgi.py
+++ studentpost/wsgi.py
@@ -11,6 +11,9 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'studentpost.settings')
+#os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'studentpost.settings')
+settings_module = "studentpost.production" if 'WEBSITE_HOSTNAME' in os.environ else 'studentpost.settings'
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
+
 
 application = get_wsgi_application()
