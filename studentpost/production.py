f49360e012758f7d6051f19e99c4c2c899e6cbac
shadabahmad01 <shdbtp322@gmail.com>
2022-02-14 20:52:31 +0530

    settings changes

--- studentpost/production.py
+++ studentpost/production.py
@@ -4,7 +4,7 @@ import os
 # Configure the domain name using the environment variable
 # that Azure automatically creates for us.
 ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']] if 'WEBSITE_HOSTNAME' in os.environ else []
-DEBUG = False
+DEBUG = os.environ['DEBUG']
 
 # WhiteNoise configuration
 MIDDLEWARE = [                                                                   
