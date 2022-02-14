346c94af4710e243441b91d06acc7de6188c7155
shadabahmad01 <shdbtp322@gmail.com>
2022-02-14 20:57:58 +0530

    fixed error

--- studentpost/production.py
+++ studentpost/production.py
@@ -4,7 +4,6 @@ import os
 # Configure the domain name using the environment variable
 # that Azure automatically creates for us.
 ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']] if 'WEBSITE_HOSTNAME' in os.environ else []
-DEBUG = os.environ['DEBUG']
 
 # WhiteNoise configuration
 MIDDLEWARE = [                                                                   
